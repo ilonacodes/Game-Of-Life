d4fd26aa35c5bafa3bc103db873466761032d45e
Ilona Demidenko <demiluri@gmail.com>
2017-07-07 22:06:14 +0200

    Fix bug with classes for cells

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 export const Cell = props => {
-    return <td className={props.status == 'alive' ? 'alive' : ''}
-               onClick={props.handleClick}></td>
+    return <td className={props.status === 'alive' ? 'alive' : ''}
+               onClick={props.handleClick}>
+
+    </td>
 };
